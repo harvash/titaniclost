ae7d997ef71a8b570cab5682a27b933267ddd45f
cxa791 <christopher.anderson@sherwin.com>
2020-02-26 14:53:26 -0500

    update sql calls

--- surviveAgeGenderSQL.php
+++ surviveAgeGenderSQL.php
@@ -21,6 +21,7 @@
                     SUM(CASE WHEN Sex = 'female' THEN 1 ELSE 0 END) AS Males,
                     SUM(CASE WHEN Sex = 'male' THEN 1 ELSE 0 END) AS Females
                     FROM titanic.titanic3 t 
+                    WHERE survived = 1
                     GROUP BY CASE
                         when age between 0 and 17 then '0-17'
                         when age between 17 and 25 then '18-25'
